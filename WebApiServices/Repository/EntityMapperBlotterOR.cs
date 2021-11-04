8c6a793962fc5f50daab0bf56558824cc598955b
zohaib4m2010 <zohaib4m2010@gmail.com>
2021-11-04 16:20:35 +0500

    update

---
+++ WebApiServices/Repository/EntityMapperBlotterOR.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiServices.Repository
+{
+    public class EntityMapperBlotterOR<TSource, TDestination> where TSource : class where TDestination : class
+    {
+        public EntityMapperBlotterOR()
+        {
+
+            Mapper.CreateMap<Models.SBP_BlotterOutRight, DataAccessLayer.SBP_BlotterOutrights>();
+            Mapper.CreateMap<DataAccessLayer.SBP_BlotterOutrights, Models.SBP_BlotterOutRight>();
+
+            Mapper.CreateMap<Models.SBP_BlotterOutRight, DataAccessLayer.SP_GetSBPBlotterOutRright_Result>();
+            Mapper.CreateMap<DataAccessLayer.SP_GetSBPBlotterOutRright_Result, Models.SBP_BlotterOutRight>();
+
+            Mapper.CreateMap<Models.SP_GetSBPBlotterOR_Result, DataAccessLayer.SP_GetSBPBlotterOutRright_Result>();
+            Mapper.CreateMap<DataAccessLayer.SP_GetSBPBlotterOutRright_Result, Models.SP_GetSBPBlotterOR_Result>();
+        }
+        public TDestination Translate(TSource obj)
+        {
+            return Mapper.Map<TDestination>(obj);
+        }
+    }
+}
