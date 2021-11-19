c013693dfd990c360d3813fe165003a15d2f81cf
imrbpro <mrb5only>
2021-11-19 11:05:08 +0500

    auto change in ef desiger

--- DataAccessLayer/BlotterEF.Designer.cs
+++ DataAccessLayer/BlotterEF.Designer.cs
@@ -1,4 +1,6 @@
+
 ﻿// T4 code generation is enabled for model 'D:\Projects\Appxone\HBLServices\DataAccessLayer\BlotterEF.edmx'. 
+
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
