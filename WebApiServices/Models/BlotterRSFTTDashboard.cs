f14742ce8acec3fe25887fbcae6f709b43d843dc
Shakir-628 <mshakir628@gmail.com>
2021-11-22 12:51:14 +0500

    Fawzan RSF

---
+++ WebApiServices/Models/BlotterRSFTTDashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiServices.Models
+{
+    public class BlotterRSFTTDashboard
+    { 
+        public string Description { get; set; }
+        public Nullable<System.DateTime> TBO_Date { get; set; }
+        public Nullable<decimal> TBO_InFlow { get; set; }
+        public Nullable<decimal> TBO_OutFlow { get; set; }
+        public long SNo { get; set; }
+    }
+}
