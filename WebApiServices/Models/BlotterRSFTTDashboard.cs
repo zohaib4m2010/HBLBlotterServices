23395be68f0703e57d4befcac56d13a524a432e1
imrbpro <mrb5only>
2021-11-30 20:06:37 +0500

    TTID added in RSFTT Model

--- WebApiServices/Models/BlotterRSFTTDashboard.cs
+++ WebApiServices/Models/BlotterRSFTTDashboard.cs
@@ -9,6 +9,7 @@ namespace WebApiServices.Models
     { 
         public string Description { get; set; }
         public Nullable<System.DateTime> TBO_Date { get; set; }
+        public int TTID { get; set; }
         public Nullable<decimal> TBO_InFlow { get; set; }
         public Nullable<decimal> TBO_OutFlow { get; set; }
         public long SNo { get; set; }
